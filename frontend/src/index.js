ec069a56bc37418e139139c745cf6530f00f7115
Jason Tan <jastan313@gmail.com>
2017-12-29 23:03:23 -0800

    Started on index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,83 @@
+
+import React, { Component } from 'react';
+import UserService from './UserService';
+import RatingService from './RatingService';
+import axios from 'axios';
+import ListTodoRow from './ListTodoRow';
+
+export default class IndexItem extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          user: null,
+          ratings: [],
+          filters: []
+      };
+      this.userService = new UserService();
+      this.ratingService = new RatingService();
+
+      //bind
+      this.onDelete = this.onDelete.bind(this);
+      this.onUpdate = this.onUpdate.bind(this);
+      this.handleAdd = this.handleAdd.bind(this);
+    }
+    componentWillMount(){
+      this.fillData();
+    }
+
+    fillData() {
+      var thisRef = this;
+      this.userService.get((data)=>{
+          thisRef.setState({ items: data });
+      })
+    }
+
+    tabRow(){
+      if(this.state.items instanceof Array){
+
+        var thisRef = this;
+        return this.state.items.map(function(object, i){
+            return <ListTodoRow onDelete={thisRef.onDelete} onUpdate={thisRef.onUpdate} obj={object} key={i} />;
+        })
+      }
+    }
+
+    onDelete(event) {
+      let id = event.target.id;
+      var thisRef = this;
+      this.todoService.delete(id,()=>{
+        thisRef.fillData();
+      });
+    }
+
+    onUpdate(event) {
+      let id = event.target.id;
+      this.props.history.push('/update/'+id);
+    }
+
+    handleAdd() {
+      this.props.history.push('/add');
+    }
+
+    render() {
+      return (
+        <div className="container">
+          <div className="panel panel-default">
+            <div className="panel-heading">List of Tasks</div>
+            <div className="panel-body">
+            <p>Click on the task description to edit</p>
+              <table id="todo-list" className="table table-bordered">
+                <tbody>
+                  {this.tabRow()}
+                </tbody>
+              </table>
+            </div>
+            <div className="panel-footer">
+              <button onClick={this.handleAdd} className="btn btn-info">New task</button>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
